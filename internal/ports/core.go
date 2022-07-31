91fff3da39478f64f7f1b96feb26d32223890c82
somerongit <someron.bakuli05@gmail.com>
2022-07-31 22:05:52 +0530

    Files added

--- internal/ports/core.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-type ArithemetictPort interface {
-	Adddition(num, num1 int32) (int32, error)
-	Subtraction(num, num1 int32) (int32, error)
-	Multiplication(num, num1 int32) (int32, error)
-	Division(num, num1 int32) (int32, error)
-}
